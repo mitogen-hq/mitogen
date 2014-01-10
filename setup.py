4666cbb435fcfa29e32d9ae4919f79f40e69580c
David Wilson <dw@botanicus.net>
2014-01-10 21:37:04 +0000

    setup.py.

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from __future__ import absolute_import
+from __future__ import with_statement
+
+from distutils.core import setup
+
+setup(
+    name = 'econtext',
+    version = '0.1',
+    description = "Remote Code Execution Contexts",
+    author = 'David Wilson',
+    license = 'OpenLDAP BSD',
+    url = 'http://github.com/dw/econtext/',
+    modules = ['econtext'],
+    zip_safe = False
+)
