081565358dd5f873ce7358305272eb23f9039bf3
David Wilson <dw@botanicus.net>
2016-08-10 22:13:57 +0100

    utils.run_with_broker.

--- econtext/utils.py
+++ econtext/utils.py
@@ -2,13 +2,16 @@
 import econtext
 
 
+def run_with_broker(func, *args, **kwargs):
+    broker = econtext.Broker()
+    try:
+        return func(broker, *args, **kwargs)
+    finally:
+        broker.Finalize()
+
+
 def with_broker(func):
     def wrapper(*args, **kwargs):
-        broker = econtext.Broker()
-        try:
-            return func(broker, *args, **kwargs)
-        finally:
-            broker.Finalize()
-
+        return run_with_broker(*args, **kwargs)
     wrapper.func_name = func.func_name
     return wrapper
