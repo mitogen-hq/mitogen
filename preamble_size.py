bad745515ceb16dd78b085a5ffa23f08384cc739
David Wilson <dw@botanicus.net>
2016-11-01 11:38:24 +0000

    Measure new modules too.

--- preamble_size.py
+++ preamble_size.py
@@ -8,6 +8,8 @@ import zlib
 
 import econtext.master
 import econtext.ssh
+import econtext.sudo
+import econtext.proxy
 
 context = econtext.master.Context(None, name='default', hostname='default')
 stream = econtext.ssh.Stream(context)
@@ -17,5 +19,11 @@ print 'Preamble size: %s (%.2fKiB)' % (
     len(stream.get_preamble()) / 1024.0,
 )
 
-master = len(zlib.compress(econtext.master.minimize_source(inspect.getsource(econtext.master))))
-print 'econtext.master size: %s (%.2fKiB)' % (master, master / 1024.0)
+for mod in (
+        econtext.master,
+        econtext.ssh,
+        econtext.sudo,
+        econtext.proxy
+        ):
+    sz = len(zlib.compress(econtext.master.minimize_source(inspect.getsource(mod))))
+    print '%s size: %s (%.2fKiB)' % (mod.__name__, sz, sz / 1024.0)
