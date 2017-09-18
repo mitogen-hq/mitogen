502266f115b249df9a841809956265a282958b24
David Wilson <dw@botanicus.net>
2017-09-18 14:34:05 +0530

    Fix Channel constructor and add simple test; closes #32

---
+++ tests/channel_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+import mitogen.core
+import testlib
+
+
+class ConstructorTest(testlib.RouterMixin, unittest.TestCase):
+    def test_constructor(self):
+        # issue 32
+        l1 = self.router.local()
+        chan = mitogen.core.Channel(self.router, l1, 123)
+        assert chan.router == self.router
+        assert chan.context == l1
+        assert chan.dst_handle == 123
+        assert chan.handle is not None
+        assert chan.handle > 0
