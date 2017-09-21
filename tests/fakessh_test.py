5dff221842ebb29a7b42fce3d949a1e122b9f846
David Wilson <dw@botanicus.net>
2017-09-21 13:06:59 +0530

    fakessh: cleanup master test.

--- tests/fakessh_test.py
+++ tests/fakessh_test.py
@@ -3,7 +3,6 @@ import os
 import shutil
 import unittest
 
-import mitogen.utils
 import mitogen.fakessh
 
 import testlib
@@ -12,11 +11,16 @@ import testlib
 class RsyncTest(testlib.DockerMixin, unittest.TestCase):
     def test_rsync_from_master(self):
         context = self.docker_ssh_any()
-        context.call(shutil.rmtree, '/tmp/data', ignore_errors=True)
-        mitogen.fakessh.run(context, self.router, [
+
+        if context.call(os.path.exists, '/tmp/data'):
+            context.call(shutil.rmtree, '/tmp/data')
+
+        return_code = mitogen.fakessh.run(context, self.router, [
             'rsync', '--progress', '-vvva',
             testlib.data_path('.'), 'target:/tmp/data'
         ])
+
+        assert return_code == 0
         assert context.call(os.path.exists, '/tmp/data')
         assert context.call(os.path.exists, '/tmp/data/simple_pkg/a.py')
 
