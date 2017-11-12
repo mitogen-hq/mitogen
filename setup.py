f077b5b092eb4ebfe2f102df22fc8ecf29b15bcc
David Wilson <dw@botanicus.net>
2017-11-12 19:57:03 +0530

    issue #56: fix up setup.py, bump version number.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(
     name = 'mitogen',
-    version = '0.0.0',
-    description = 'Library for writing distributed self-replicating programs. THIS PACKAGE IS INCOMPLETE. IT IS BEING UPLOADED BECAUSE PYPI MAINTAINERS BROKE THE REGISTER COMMAND',
+    version = '0.0.1',
+    description = 'Library for writing distributed self-replicating programs.',
     author = 'David Wilson',
     license = 'OpenLDAP BSD',
-    url = 'http://github.com/dw/mitogen/',
+    url = 'https://github.com/dw/mitogen/',
     packages = ['mitogen'],
     zip_safe = False,
     classifiers = [
