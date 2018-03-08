bbd8b6253bd4a47742f563070579a1c5c55b26f9
Alex Willmer <alex@moreati.org.uk>
2018-03-08 21:35:38 +0000

    tests: Find all *_test.py scripts and run them
    Don't rely on maintaining a list of test files, it's too easy to forget
    one and silently fail to run it.

--- test.sh
+++ test.sh
@@ -22,18 +22,7 @@ sys.exit(1)
     ' "$@"
 }
 
-timeout 05.0 python tests/call_function_test.py
-timeout 05.0 python tests/channel_test.py
-timeout 05.0 python tests/first_stage_test.py
-timeout 05.0 python tests/id_allocation_test.py
-timeout 05.0 python tests/importer_test.py
-timeout 05.0 python tests/local_test.py
-timeout 05.0 python tests/master_test.py
-timeout 05.0 python tests/module_finder_test.py
-timeout 05.0 python tests/nested_test.py
-timeout 05.0 python tests/parent_test.py
-timeout 05.0 python tests/responder_test.py
-timeout 05.0 python tests/utils_test.py
-timeout 20.0 python tests/select_test.py
-timeout 20.0 python tests/ssh_test.py
-timeout 30.0 python tests/fakessh_test.py
+for f in tests/*_test.py; do
+    echo $f
+    timeout 10 python $f
+done
