443c94eb3925231d6d0167d5ccb7fab335038a9e
David Wilson <dw@botanicus.net>
2018-03-25 12:34:39 +0545

    issue #155: core: prevent set_cloexec() use on standard handles

--- mitogen/core.py
+++ mitogen/core.py
@@ -185,6 +185,7 @@ def is_blacklisted_import(importer, fullname):
 
 def set_cloexec(fd):
     flags = fcntl.fcntl(fd, fcntl.F_GETFD)
+    assert fd > 2
     fcntl.fcntl(fd, fcntl.F_SETFD, flags | fcntl.FD_CLOEXEC)
 
 
