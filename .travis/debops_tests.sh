058ddeee581abefe7c1187287fa77ac7ef874385
David Wilson <dw@botanicus.net>
2018-04-04 12:59:22 +0100

    issue #164: run against 4 targets.

--- .travis/debops_tests.sh
+++ .travis/debops_tests.sh
@@ -6,10 +6,12 @@ TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-`pwd`}"
 
 function on_exit()
 {
+    echo travis_fold:start:cleanup
     [ "$KEEP" ] || {
         rm -rvf "$TMPDIR" || true
         docker kill target || true
     }
+    echo travis_fold:end:cleanup
 }
 
 trap on_exit EXIT
@@ -17,7 +19,10 @@ mkdir "$TMPDIR"
 
 
 echo travis_fold:start:docker_setup
-docker run --rm --detach --name=target d2mw/mitogen-test /bin/sleep 86400
+docker run --rm --detach --name=target1 d2mw/mitogen-test /bin/sleep 86400
+docker run --rm --detach --name=target2 d2mw/mitogen-test /bin/sleep 86400
+docker run --rm --detach --name=target3 d2mw/mitogen-test /bin/sleep 86400
+docker run --rm --detach --name=target4 d2mw/mitogen-test /bin/sleep 86400
 echo travis_fold:end:docker_setup
 
 
@@ -32,7 +37,7 @@ strategy_plugins = ${TRAVIS_BUILD_DIR}/ansible_mitogen/plugins/strategy
 strategy = mitogen_linear
 EOF
 
-cat > ansible/inventory/host_vars/target.yml <<-EOF
+cat > ansible/inventory/group_vars/debops_all_hosts.yml <<-EOF
 ansible_connection: docker
 ansible_python_interpreter: /usr/bin/python2.7
 
@@ -40,7 +45,12 @@ ansible_python_interpreter: /usr/bin/python2.7
 dhparam__bits: ["128", "64"]
 EOF
 
-echo target >> ansible/inventory/hosts
+cat  ansible/inventory/hosts <<-EOF
+target1
+target2
+target3
+target4
+EOF
 echo travis_fold:end:job_setup
 
 
