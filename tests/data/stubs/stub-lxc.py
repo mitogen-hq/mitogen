0e8f4511900d12bf988ebbc91e4eab6c74c98492
David Wilson <dw@botanicus.net>
2018-10-31 12:12:17 +0000

    issue #409: add stub LXC test, refactor playbooks.

--- tests/data/stubs/stub-lxc.py
+++ tests/data/stubs/stub-lxc.py
@@ -3,5 +3,10 @@
 import sys
 import os
 
+# setns.py fetching leader PID.
+if sys.argv[1] == 'info':
+    print 'Pid: 1'
+    sys.exit(0)
+
 os.environ['ORIGINAL_ARGV'] = repr(sys.argv)
 os.execv(sys.executable, sys.argv[sys.argv.index('--') + 1:])
