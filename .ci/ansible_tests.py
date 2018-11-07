e12c963279c22a8ef9e3100dfbac405ffcb4dc42
David Wilson <dw@botanicus.net>
2018-11-07 00:52:20 +0000

    issue #426: make ansible_tests use python_path

--- .ci/ansible_tests.py
+++ .ci/ansible_tests.py
@@ -32,12 +32,14 @@ with ci_lib.Fold('job_setup'):
     run("mkdir %s", HOSTS_DIR)
     run("ln -s %s/hosts/common-hosts %s", TESTS_DIR, HOSTS_DIR)
 
-    with open(os.path.join(HOSTS_DIR, 'target'), 'w') as fp:
+    inventory_path = os.path.join(HOSTS_DIR, 'target')
+    with open(inventory_path, 'w') as fp:
         fp.write('[test-targets]\n')
         fp.writelines(
             "%(name)s "
             "ansible_host=%(hostname)s "
             "ansible_port=%(port)s "
+            "ansible_python_interpreter=%(python_path)s "
             "ansible_user=mitogen__has_sudo_nopw "
             "ansible_password=has_sudo_nopw_password"
             "\n"
