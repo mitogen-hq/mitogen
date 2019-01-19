d2eb01f793a21ab5bbe77f7161468bd5d05b1c08
David Wilson <dw@botanicus.net>
2019-01-19 06:44:49 +0000

    tests: pin idna to last supporting 2.6-compat version.

--- .ci/ansible_install.py
+++ .ci/ansible_install.py
@@ -6,7 +6,7 @@ batches = [
     [
         # Must be installed separately, as PyNACL indirect requirement causes
         # newer version to be installed if done in a single pip run.
-        'pip install "pycparser<2.19"',
+        'pip install "pycparser<2.19" "idna<2.7"',
         'pip install '
             '-r tests/requirements.txt '
             '-r tests/ansible/requirements.txt',
