1555f232153b5904b67e924275e5d39ff506adf2
David Wilson <dw@botanicus.net>
2019-01-21 15:25:46 +0000

    tests: add some more helper function tests.

--- tests/call_error_test.py
+++ tests/call_error_test.py
@@ -16,15 +16,18 @@ class ConstructorTest(testlib.TestCase):
     def test_string_noargs(self):
         e = self.klass('%s%s')
         self.assertEquals(e.args[0], '%s%s')
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
 
     def test_string_args(self):
         e = self.klass('%s%s', 1, 1)
         self.assertEquals(e.args[0], '11')
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
 
     def test_from_exc(self):
         ve = plain_old_module.MyError('eek')
         e = self.klass(ve)
         self.assertEquals(e.args[0], 'plain_old_module.MyError: eek')
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
 
     def test_form_base_exc(self):
         ve = SystemExit('eek')
@@ -32,6 +35,7 @@ class ConstructorTest(testlib.TestCase):
         self.assertEquals(e.args[0],
             # varies across 2/3.
             '%s.%s: eek' % (type(ve).__module__, type(ve).__name__))
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
 
     def test_from_exc_tb(self):
         try:
@@ -41,7 +45,38 @@ class ConstructorTest(testlib.TestCase):
             e = self.klass(ve)
 
         self.assertTrue(e.args[0].startswith('plain_old_module.MyError: eek'))
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
         self.assertTrue('test_from_exc_tb' in e.args[0])
+
+    def test_bytestring_conversion(self):
+        e = self.klass(mitogen.core.b('bytes'))
+        self.assertEquals(u'bytes', e.args[0])
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
+
+    def test_reduce(self):
+        e = self.klass('eek')
+        func, (arg,) = e.__reduce__()
+        self.assertTrue(func is mitogen.core._unpickle_call_error)
+        self.assertEquals(arg, e.args[0])
+
+
+class UnpickleCallErrorTest(testlib.TestCase):
+    func = staticmethod(mitogen.core._unpickle_call_error)
+
+    def test_not_unicode(self):
+        self.assertRaises(TypeError,
+            lambda: self.func(mitogen.core.b('bad')))
+
+    def test_oversized(self):
+        self.assertRaises(TypeError,
+            lambda: self.func(mitogen.core.b('b'*10001)))
+
+    def test_reify(self):
+        e = self.func(u'some error')
+        self.assertEquals(mitogen.core.CallError, type(e))
+        self.assertEquals(1, len(e.args))
+        self.assertEquals(mitogen.core.UnicodeType, type(e.args[0]))
+        self.assertEquals(u'some error', e.args[0])
 
 
 class PickleTest(testlib.TestCase):
---
+++ tests/error_test.py
@@ -0,0 +1,40 @@
+
+import logging
+import subprocess
+import time
+import zlib
+
+import unittest2
+
+import testlib
+import mitogen.master
+import mitogen.parent
+import mitogen.utils
+
+
+class ConstructorTest(testlib.TestCase):
+    klass = mitogen.core.Error
+
+    def test_literal_no_format(self):
+        e = self.klass('error')
+        self.assertEquals(e.args[0], 'error')
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
+
+    def test_literal_format_chars_present(self):
+        e = self.klass('error%s')
+        self.assertEquals(e.args[0], 'error%s')
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
+
+    def test_format(self):
+        e = self.klass('error%s', 123)
+        self.assertEquals(e.args[0], 'error123')
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
+
+    def test_bytes_to_unicode(self):
+        e = self.klass(mitogen.core.b('error'))
+        self.assertEquals(e.args[0], 'error')
+        self.assertTrue(isinstance(e.args[0], mitogen.core.UnicodeType))
+
+
+if __name__ == '__main__':
+    unittest2.main()
