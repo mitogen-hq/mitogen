f36b4b47bf6e73f0c2c87b0ad8264ba1d98311c5
David Wilson <dw@botanicus.net>
2019-03-06 11:22:40 +0000

    issue #554: don't rely on tmp_path autoremoval in test.
    Ansible doesn't do this, so we shouldn't either.

--- tests/ansible/lib/action/mitogen_action_script.py
+++ tests/ansible/lib/action/mitogen_action_script.py
@@ -16,12 +16,20 @@ def execute(s, gbls, lcls):
 class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
         super(ActionModule, self).run(tmp=tmp, task_vars=task_vars)
-        lcls = {
-            'self': self,
-            'result': {}
-        }
+
+        lcls = {}
+        # Capture keys to remove later, including any crap Python adds.
+        execute('pass', globals(), lcls)
+        lcls['self'] = self
+        # Old mitogen_action_script used explicit result dict.
+        lcls['result'] = lcls
+
+        pre_keys = list(lcls)
         execute(self._task.args['script'], globals(), lcls)
-        return lcls['result']
+
+        for key in pre_keys:
+            del lcls[key]
+        return lcls
 
 
 if __name__ == '__main__':
