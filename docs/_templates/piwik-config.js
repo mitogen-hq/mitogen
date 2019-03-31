2758c38f4f939b1b71555a24a2e8cd191ec4423b
David Wilson <dw@botanicus.net>
2019-03-31 05:07:40 +0100

    docs: fix config var scope

--- docs/_templates/piwik-config.js
+++ docs/_templates/piwik-config.js
@@ -1,5 +1,5 @@
-var _paq = _paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-_paq.push(['enableHeartBeatTimer', 30]);
-_paq.push(['setSiteId', 6]);
+window._paq = [];
+window._paq.push(['trackPageView']);
+window._paq.push(['enableLinkTracking']);
+window._paq.push(['enableHeartBeatTimer', 30]);
+window._paq.push(['setSiteId', 6]);
