dc2e9e2dd18657256cfb419c8f320fdb8d12697d
David Wilson <dw@botanicus.net>
2019-04-01 03:01:02 +0100

    import redirector

---
+++ docs/_templates/piwik-config.js
@@ -0,0 +1,5 @@
+window._paq = [];
+window._paq.push(['trackPageView']);
+window._paq.push(['enableLinkTracking']);
+window._paq.push(['enableHeartBeatTimer', 30]);
+window._paq.push(['setSiteId', 6]);
---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+author = u'David Wilson'
+copyright = u'2019, David Wilson'
+exclude_patterns = ['_build']
+html_additional_pages = {
+    'ansible': 'ansible.html',
+    'ansible_detailed': 'ansible_detailed.html',
+    'api': 'api.html',
+    'changelog': 'changelog.html',
+    'contributors': 'contributors.html',
+    'examples': 'examples.html',
+    'getting_started': 'getting_started.html',
+    'howitworks': 'howitworks.html',
+    'index': 'index.html',
+    'internals': 'internals.html',
+    'services': 'services.html',
+    'signals': 'signals.html',
+    'toc': 'toc.html',
+}
+
+html_theme = 'basic'
+language = None
+master_doc = 'placeholder'
+project = u'Mitogen'
+pygments_style = 'sphinx'
+release = '0.0'
+source_suffix = '.rst'
+templates_path = ['_templates']
+version = '0.0'
