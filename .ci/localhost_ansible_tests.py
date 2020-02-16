5c27e07d3cccf87d2bb34affc918f9d133f3ba83
Steven Robertson <srtrumpetaggie@gmail.com>
2020-02-16 00:52:49 -0800

    adding verbosity

--- .ci/localhost_ansible_tests.py
+++ .ci/localhost_ansible_tests.py
@@ -50,5 +50,5 @@ with ci_lib.Fold('machine_prep'):
 with ci_lib.Fold('ansible'):
     os.chdir(TESTS_DIR)
     playbook = os.environ.get('PLAYBOOK', 'all.yml')
-    run('./run_ansible_playbook.py %s -l target %s',
+    run('./run_ansible_playbook.py %s -l target %s -vvv',
         playbook, ' '.join(sys.argv[1:]))
