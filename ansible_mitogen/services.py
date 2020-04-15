afa7c252e7e96c63182b573707b0dbfea38835af
michaelzh <zhongwm@gmail.com>
2020-04-15 14:54:38 +0800

    Update services.py

--- ansible_mitogen/services.py
+++ ansible_mitogen/services.py
@@ -182,15 +182,17 @@ class ContextService(mitogen.service.Service):
                     return False
 
                 context = response['context']
+            if context:
+                mitogen.core.listen(context, 'disconnect', l.put)
+                self._shutdown_unlocked(context)
 
-            mitogen.core.listen(context, 'disconnect', l.put)
-            self._shutdown_unlocked(context)
-
-        # The timeout below is to turn a hang into a crash in case there is any
-        # possible race between 'disconnect' signal subscription, and the child
-        # abruptly disconnecting.
-        l.get(timeout=30.0)
-        return True
+                # The timeout below is to turn a hang into a crash in case there is any
+                # possible race between 'disconnect' signal subscription, and the child
+                # abruptly disconnecting.
+                l.get(timeout=30.0)
+                return True
+            else:
+                return False
 
     @mitogen.service.expose(mitogen.service.AllowParents())
     @mitogen.service.arg_spec({
