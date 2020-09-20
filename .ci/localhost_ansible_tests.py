40d53bcf9671a8c4e556d8b798d13a4a13521480
Steven Robertson <srtrumpetaggie@gmail.com>
2020-09-20 16:33:41 -0700

    3 v is too much v for azure devops to render

--- .ci/localhost_ansible_tests.py
+++ .ci/localhost_ansible_tests.py
@@ -46,5 +46,5 @@ with ci_lib.Fold('machine_prep'):
 with ci_lib.Fold('ansible'):
     os.chdir(TESTS_DIR)
     playbook = os.environ.get('PLAYBOOK', 'all.yml')
-    run('./run_ansible_playbook.py %s -l target %s -vvv',
+    run('./run_ansible_playbook.py %s -l target %s -vv',
         playbook, ' '.join(sys.argv[1:]))
