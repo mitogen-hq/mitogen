0db7467e4a68e4239afcf411a65946bc7809c431
Alex Willmer <alex@moreati.org.uk>
2021-09-08 02:18:27 +0100

    tests: Authenticate to AWS Elastic Container Registry for `docker pull`
    This should increase the rate limit from 1/s to 10/s. AWS
    credentials are obtained from environment variables in Azure
    pipelines, or the default AWS CLI profile.
    
    https://docs.aws.amazon.com/AmazonECR/latest/public/public-service-quotas.html
    https://docs.aws.amazon.com/AmazonECR/latest/public/public-registries.html#public-registry-auth
    https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html

--- .ci/ansible_install.py
+++ .ci/ansible_install.py
@@ -13,12 +13,15 @@ batches = [
             '-r tests/ansible/requirements.txt',
         # encoding is required for installing ansible 2.10 with pip2, otherwise we get a UnicodeDecode error
         'LC_CTYPE=en_US.UTF-8 LANG=en_US.UTF-8 pip install -q ansible=={0}'.format(ci_lib.ANSIBLE_VERSION)
+    ],
+    [
+        'aws ecr-public get-login-password | docker login --username AWS --password-stdin public.ecr.aws',
     ]
 ]
 
-batches.append(ci_lib.throttle(
+batches[-1].extend([
     'docker pull %s' % (ci_lib.image_for_distro(distro),)
     for distro in ci_lib.DISTROS
-))
+])
 
 ci_lib.run_batches(batches)
--- .ci/debops_common_install.py
+++ .ci/debops_common_install.py
@@ -13,6 +13,7 @@ ci_lib.run_batches([
         'pip install -qqq debops[ansible]==2.1.2 ansible==%s' % ci_lib.ANSIBLE_VERSION,
     ],
     [
+        'aws ecr-public get-login-password | docker login --username AWS --password-stdin public.ecr.aws',
         'docker pull %s' % (ci_lib.image_for_distro('debian'),),
     ],
 ])
