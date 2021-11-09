39d02e9d5932cc0cdbb718ea19e89eec75d1947d
Alex Willmer <alex@moreati.org.uk>
2021-11-09 19:28:00 +0000

    ci: Don't install pycparser and idna seperately
    AFAICT no longer necessary. These install fine as part of requirements.txt.

--- .ci/debops_common_install.py
+++ .ci/debops_common_install.py
@@ -7,9 +7,6 @@ ci_lib.DISTROS = ['debian']
 
 ci_lib.run_batches([
     [
-        # Must be installed separately, as PyNACL indirect requirement causes
-        # newer version to be installed if done in a single pip run.
-        'pip install "pycparser<2.19"',
         'pip install -qqq "debops[ansible]==2.1.2" "ansible-base<2.10.14" "ansible=={}"'.format(ci_lib.ANSIBLE_VERSION),
     ],
     [
