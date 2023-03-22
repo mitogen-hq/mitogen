ec05e542b46dcfc2a418943c105f03dcfb7c5b69
Philippe Kueck <bqobccy6ejnq2bqvmebqiwqha4cs4@protected32.unixadm.org>
2023-03-22 12:36:14 +0100

    Fix 'ansible_host_key_checking' and 'ansible_ssh_host_key_checking' for adding new hosts to the inventory using 'add_hosts'
    Co-authored-by: Alex Willmer <alex@moreati.org.uk>

--- ansible_mitogen/transport_config.py
+++ ansible_mitogen/transport_config.py
@@ -67,6 +67,7 @@ import ansible.utils.shlex
 import ansible.constants as C
 
 from ansible.module_utils.six import with_metaclass
+from ansible.module_utils.parsing.convert_bool import boolean
 
 # this was added in Ansible >= 2.8.0; fallback to the default interpreter if necessary
 try:
@@ -243,6 +244,12 @@ class Spec(with_metaclass(abc.ABCMeta, object)):
     def python_path(self):
         """
         Path to the Python interpreter on the target machine.
+        """
+
+    @abc.abstractmethod
+    def host_key_checking(self):
+        """
+        Whether or not to check the keys of the target machine
         """
 
     @abc.abstractmethod
@@ -465,6 +472,14 @@ class PlayContextSpec(Spec):
             task_vars=self._task_vars,
             action=self._action,
             rediscover_python=rediscover_python)
+
+    def host_key_checking(self):
+        def candidates():
+            yield self._connection.get_task_var('ansible_ssh_host_key_checking')
+            yield self._connection.get_task_var('ansible_host_key_checking')
+            yield C.HOST_KEY_CHECKING
+        val = next((v for v in candidates() if v is not None), True)
+        return boolean(val)
 
     def private_key_file(self):
         return self._play_context.private_key_file
@@ -691,6 +706,14 @@ class MitogenViaSpec(Spec):
             task_vars=self._task_vars,
             action=self._action,
             rediscover_python=rediscover_python)
+
+    def host_key_checking(self):
+        def candidates():
+            yield self._host_vars.get('ansible_ssh_host_key_checking')
+            yield self._host_vars.get('ansible_host_key_checking')
+            yield C.HOST_KEY_CHECKING
+        val = next((v for v in candidates() if v is not None), True)
+        return boolean(val)
 
     def private_key_file(self):
         # TODO: must come from PlayContext too.
