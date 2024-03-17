92c00d913ee57e2d0e965a996c3bb74c569ca0cb
Alex Willmer <alex@moreati.org.uk>
2024-03-17 14:56:13 +0000

    tests: Skip "discovered python matches invoked" on macOS 11/Python 2.7/Vanilla

--- tests/ansible/lib/modules/test_echo_module.py
+++ tests/ansible/lib/modules/test_echo_module.py
@@ -23,11 +23,11 @@ def main():
 
     # revert the Mitogen OSX tweak since discover_interpreter() doesn't return this info
     # NB This must be synced with mitogen.parent.Connection.get_boot_command()
+    platform_release_major = int(platform.release().partition('.')[0])
     if sys.modules.get('mitogen') and sys.platform == 'darwin':
-        darwin_major = int(platform.release().partition('.')[0])
-        if darwin_major < 19 and sys.executable == '/usr/bin/python2.7':
+        if platform_release_major < 19 and sys.executable == '/usr/bin/python2.7':
             sys.executable = '/usr/bin/python'
-        if darwin_major in (20, 21) and sys.version_info[:2] == (2, 7):
+        if platform_release_major in (20, 21) and sys.version_info[:2] == (2, 7):
             # only for tests to check version of running interpreter -- Mac 10.15+ changed python2
             # so it looks like it's /usr/bin/python but actually it's /System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
             sys.executable = "/usr/bin/python"
@@ -46,12 +46,21 @@ def main():
             'resolved': os.path.realpath(discovered_interpreter_python),
         },
         'running_python': {
+            'platform': {
+                'release': {
+                    'major': platform_release_major,
+                },
+            },
             'sys': {
                 'executable': {
                     'as_seen': sys.executable,
                     'resolved': os.path.realpath(sys.executable),
                 },
                 'platform': sys.platform,
+                'version_info': {
+                    'major': sys.version_info[0],
+                    'minor': sys.version_info[1],
+                },
             },
         },
     }
