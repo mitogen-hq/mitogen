2edcb31996b894e1683c5c23114f944572d97690
Alex Willmer <alex@moreati.org.uk>
2024-07-31 08:30:38 +0100

    docs: Remove piwik analytics hooks from website
    https://networkgenomics.com is no longer served, so the javascript and other
    attempts to beacon or redirect result in HTTP errors.

--- docs/_templates/piwik-config.js
+++
@@ -1,5 +0,0 @@
-window._paq = [];
-window._paq.push(['trackPageView']);
-window._paq.push(['enableLinkTracking']);
-window._paq.push(['enableHeartBeatTimer', 30]);
-window._paq.push(['setSiteId', 6]);
