8613f685abdfe5cbe192221eb14ec26a33d3683b
Alex Willmer <alex@moreati.org.uk>
2024-08-10 19:37:25 +0100

    tests: Skip AWS ECR login outside of CI jobs
    To avoid rate limiting errors, CI (currently Azure Devops) logs into the
    container registry (currently AWS ECR). Outside CI this is unnnecessary and
    makes it harder to run the tests, because very few people have access to a
    suitable AWS secret token.
    
    Following this change `aws ecr-public get-login-password` will only be run if
    the environment variable $TF_BUILD==True. This is set by Azure Pipelines
    jobs. If the CI platform is changed then another indicator should be used.
    
    https://adamj.eu/tech/2020/03/09/detect-if-your-tests-are-running-on-ci/

--- .ci/mitogen_py24_install.py
+++ .ci/mitogen_py24_install.py
@@ -4,7 +4,7 @@ import ci_lib
 
 batches = [
     [
-        'aws ecr-public get-login-password | docker login --username AWS --password-stdin public.ecr.aws',
+        'if [ "${TF_BUILD:-false}" = "True" ]; then aws ecr-public get-login-password | docker login --username AWS --password-stdin public.ecr.aws; fi',
     ],
     [
         'curl https://dw.github.io/mitogen/binaries/ubuntu-python-2.4.6.tar.bz2 | sudo tar -C / -jxv',
