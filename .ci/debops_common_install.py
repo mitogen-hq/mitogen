b926795973906d77918843bf5f750f5c92315204
Alex Willmer <alex@moreati.org.uk>
2024-09-24 18:04:18 +0100

    ci: Move container registry authentication to an Azure Devops step
    This aims to
    - Reduce duplication
    - Seperate CI specific setup from test setup
    - Prepare for migration from Azure DevOps to GitHub Actions

--- .ci/debops_common_install.py
+++ .ci/debops_common_install.py
@@ -9,9 +9,6 @@ ci_lib.run_batches([
     [
         'python -m pip --no-python-version-warning --disable-pip-version-check "debops[ansible]==2.1.2"',
     ],
-    [
-        'if [ "${TF_BUILD:-false}" = "True" ]; then aws ecr-public get-login-password | docker login --username AWS --password-stdin public.ecr.aws; fi',
-    ],
 ])
 
 ci_lib.run('ansible-galaxy collection install debops.debops:==2.1.2')
