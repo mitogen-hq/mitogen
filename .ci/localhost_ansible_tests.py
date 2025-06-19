619cbbe3ac6bcb3253ffe069de8cab7c51dca86a
Alex Willmer <alex@moreati.org.uk>
2025-06-19 12:55:19 +0100

    WIP

--- .ci/localhost_ansible_tests.py
+++ .ci/localhost_ansible_tests.py
@@ -17,17 +17,6 @@ with ci_lib.Fold('unit_tests'):
 
 with ci_lib.Fold('job_setup'):
     os.chmod(ci_lib.TESTS_SSH_PRIVATE_KEY_FILE, int('0600', 8))
-    # NOTE: sshpass v1.06 causes errors so pegging to 1.05 -> "msg": "Error when changing password","out": "passwd: DS error: eDSAuthFailed\n", 
-    # there's a checksum error with "brew install http://git.io/sshpass.rb" though, so installing manually
-    if not ci_lib.exists_in_path('sshpass'):
-        subprocess.check_call(
-                "curl -O -L  https://sourceforge.net/projects/sshpass/files/sshpass/1.05/sshpass-1.05.tar.gz && \
-                tar xvf sshpass-1.05.tar.gz && \
-                cd sshpass-1.05 && \
-                ./configure && \
-                sudo make install",
-                shell=True,
-        )
 
 
 with ci_lib.Fold('machine_prep'):
