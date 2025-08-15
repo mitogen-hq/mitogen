22c20ab4409690048035b54514a7e6cb9cb049c1
Alex Willmer <alex@moreati.org.uk>
2025-08-15 17:51:17 +0100

    mitogen: Fix non-blocking IO errors in first stage of bootstrap
    When /etc/sudoers has log_output (or similar) enabled the process spawned by
    `ctx.sudo()` via `mitogen.parent.Connection.start_child()` receives a stdin
    that is in non-blocking mode. The immediate symptom is that `os.openfd(0,
    ...).read(n)` sometimes returns `None`, causing the first stage to raise an
    unhandled TypeError.
    
    The fix (for now) is to use `select.select()` in as while loop to read stdin.
    This increases the command size slightly, but I think it's a reasonable
    tradeoff until/unless the cause is more fully understood.
    
    All CI tests are now run with sudoers log_output enabled, in order to catch
    regressions. `first_stage_test.CommandLineTest` has been amended, because it
    relied on implementation details of the bootstrap process that are no longer
    true.
    
    Before
    ```
    SSH command size: 755
    Preamble (mitogen.core + econtext) size: 18227 (17.80KiB)
    
                            Original           Minimized           Compressed
    mitogen.core         152218 148.7KiB  68437 66.8KiB 45.0%  18124 17.7KiB 11.9%
    mitogen.parent        98853  96.5KiB  51103 49.9KiB 51.7%  12881 12.6KiB 13.0%
    mitogen.fork           8445   8.2KiB   4139  4.0KiB 49.0%   1652  1.6KiB 19.6%
    mitogen.ssh           10827  10.6KiB   6893  6.7KiB 63.7%   2099  2.0KiB 19.4%
    mitogen.sudo          12089  11.8KiB   5924  5.8KiB 49.0%   2249  2.2KiB 18.6%
    mitogen.select        12325  12.0KiB   2929  2.9KiB 23.8%    964  0.9KiB  7.8%
    mitogen.service       41581  40.6KiB  22398 21.9KiB 53.9%   5847  5.7KiB 14.1%
    mitogen.fakessh       15767  15.4KiB   8149  8.0KiB 51.7%   2676  2.6KiB 17.0%
    mitogen.master        55317  54.0KiB  28846 28.2KiB 52.1%   7528  7.4KiB 13.6%
    ```
    
    After
    ```
    SSH command size: 798
    Preamble (mitogen.core + econtext) size: 18227 (17.80KiB)
    
                            Original           Minimized           Compressed
    mitogen.core         152218 148.7KiB  68437 66.8KiB 45.0%  18124 17.7KiB 11.9%
    mitogen.parent        98944  96.6KiB  51180 50.0KiB 51.7%  12910 12.6KiB 13.0%
    mitogen.fork           8445   8.2KiB   4139  4.0KiB 49.0%   1652  1.6KiB 19.6%
    mitogen.ssh           10827  10.6KiB   6893  6.7KiB 63.7%   2099  2.0KiB 19.4%
    mitogen.sudo          12089  11.8KiB   5924  5.8KiB 49.0%   2249  2.2KiB 18.6%
    mitogen.select        12325  12.0KiB   2929  2.9KiB 23.8%    964  0.9KiB  7.8%
    mitogen.service       41581  40.6KiB  22398 21.9KiB 53.9%   5847  5.7KiB 14.1%
    mitogen.fakessh       15767  15.4KiB   8149  8.0KiB 51.7%   2676  2.6KiB 17.0%
    mitogen.master        55317  54.0KiB  28846 28.2KiB 52.1%   7528  7.4KiB 13.6%
    ```

--- .ci/mitogen_tests.py
+++ .ci/mitogen_tests.py
@@ -2,6 +2,7 @@
 # Run the Mitogen tests.
 
 import os
+import subprocess
 
 import ci_lib
 
@@ -12,6 +13,14 @@ os.environ.update({
 
 if not ci_lib.have_docker():
     os.environ['SKIP_DOCKER_TESTS'] = '1'
+
+subprocess.check_call(
+    "umask 0022; sudo cp '%s' '%s'"
+    % (ci_lib.SUDOERS_DEFAULTS_SRC, ci_lib.SUDOERS_DEFAULTS_DEST),
+    shell=True,
+)
+subprocess.check_call(['sudo', 'visudo', '-cf', ci_lib.SUDOERS_DEFAULTS_DEST])
+subprocess.check_call(['sudo', '-l'])
 
 interesting = ci_lib.get_interesting_procs()
 ci_lib.run('./run_tests -v')
