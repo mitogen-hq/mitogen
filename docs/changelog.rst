14e833470501302afb3b0a8ca40a70d5e67da70f
Alex Willmer <alex@moreati.org.uk>
2025-11-26 16:03:45 +0000

    CI: Add OS release coverage: Debian 12

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -25,6 +25,7 @@ In progress (unreleased)
 * :gh:issue:`1358` CI: Bump deprecated macOS 13 runner to macOS 15
 * :gh:issue:`1118` CI: Add OS release coverage: AlmaLinux 9
 * :gh:issue:`1118` CI: Add OS release coverage: CentOS 5
+* :gh:issue:`1118` CI: Add OS release coverage: Debian 12
 
 
 v0.3.33 (2025-11-22)
--- tests/testlib.py
+++ tests/testlib.py
@@ -53,7 +53,7 @@ LOG = logging.getLogger(__name__)
 
 DISTRO_SPECS = os.environ.get(
     'MITOGEN_TEST_DISTRO_SPECS',
-    'alma9-py3 centos5 centos8-py3 debian9 debian11 ubuntu1604 ubuntu2004',
+    'alma9-py3 centos5 centos8-py3 debian9 debian12-py3 ubuntu1604 ubuntu2004',
 )
 IMAGE_TEMPLATE = os.environ.get(
     'MITOGEN_TEST_IMAGE_TEMPLATE',
