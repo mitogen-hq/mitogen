0ab5b425d8258d27afd17655f7d6cca11550b9aa
Marc Hartmayer <mhartmay@linux.ibm.com>
2025-12-09 09:58:40 +0000

    first_stage_test: Refactor the test
    Use testlib.subprocess instead of subprocess and make the test description a
    docstring that can be used by the test runner.
    
    Signed-off-by: Marc Hartmayer <mhartmay@linux.ibm.com>

--- tests/first_stage_test.py
+++ tests/first_stage_test.py
@@ -1,5 +1,3 @@
-import subprocess
-
 import mitogen.core
 import mitogen.parent
 from mitogen.core import b
@@ -17,28 +15,32 @@ class CommandLineTest(testlib.RouterMixin, testlib.TestCase):
     #   * 3.x starting 2.7
 
     def test_valid_syntax(self):
+        """Test valid syntax
+
+        The boot command should write an ECO marker to stdout, read the
+        preamble from stdin, then execute it.
+
+        This test attaches /dev/zero to stdin to create a specific failure
+
+        1. Fork child reads <compressed preamble size> bytes of NUL (`b'\0'`)
+        2. Fork child crashes (trying to decompress the junk data)
+        3. Fork child's file descriptors (write pipes) are closed by the OS
+        4. Fork parent does `dup(<read pipe>, <stdin>)` and `exec(<python>)`
+        5. Python reads `b''` (i.e. EOF) from stdin (a closed pipe)
+        6. Python runs `''` (a valid script) and exits with success
+
+        """
+
         options = mitogen.parent.Options(max_message_size=123)
         conn = mitogen.parent.Connection(options, self.router)
         conn.context = mitogen.core.Context(None, 123)
-        args = conn.get_boot_command()
-
-        # The boot command should write an ECO marker to stdout, read the
-        # preamble from stdin, then execute it.
-
-        # This test attaches /dev/zero to stdin to create a specific failure
-        # 1. Fork child reads <compressed preamble size> bytes of NUL (`b'\0'`)
-        # 2. Fork child crashes (trying to decompress the junk data)
-        # 3. Fork child's file descriptors (write pipes) are closed by the OS
-        # 4. Fork parent does `dup(<read pipe>, <stdin>)` and `exec(<python>)`
-        # 5. Python reads `b''` (i.e. EOF) from stdin (a closed pipe)
-        # 6. Python runs `''` (a valid script) and exits with success
-
         fp = open("/dev/zero", "r")
         try:
-            proc = subprocess.Popen(args,
+            proc = testlib.subprocess.Popen(
+                args=conn.get_boot_command(),
                 stdin=fp,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                stdout=testlib.subprocess.PIPE,
+                stderr=testlib.subprocess.PIPE,
             )
             stdout, stderr = proc.communicate()
             self.assertEqual(0, proc.returncode)
