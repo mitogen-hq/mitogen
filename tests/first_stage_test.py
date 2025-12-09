e9f7d34bf0befd4472e4d18868e2fe180eedb4cf
Marc Hartmayer <mhartmay@linux.ibm.com>
2025-12-09 20:12:10 +0000

    first_stage_test: Add test_premature_eof test
    Signed-off-by: Marc Hartmayer <mhartmay@linux.ibm.com>

--- tests/first_stage_test.py
+++ tests/first_stage_test.py
@@ -182,3 +182,47 @@ class CommandLineTest(testlib.RouterMixin, testlib.TestCase):
             )
         finally:
             fp.close()
+
+    def test_premature_eof(self):
+        """The boot command should write an ECO marker to stdout, read the
+        preamble from stdin, then execute it.
+
+        This test writes some data to STDIN and closes it then to create an
+        EOF situation.
+        1. Fork child tries to read from STDIN, but stops as EOF is received.
+        2. Fork child crashes (trying to decompress the junk data)
+        3. Fork child's file descriptors (write pipes) are closed by the OS
+        4. Fork parent does `dup(<read pipe>, <stdin>)` and `exec(<python>)`
+        5. Python reads `b''` (i.e. EOF) from stdin (a closed pipe)
+        6. Python runs `''` (a valid script) and exits with success"""
+
+        proc = testlib.subprocess.Popen(
+            args=self.args,
+            stdout=testlib.subprocess.PIPE,
+            stderr=testlib.subprocess.PIPE,
+            stdin=testlib.subprocess.PIPE,
+        )
+
+        # Do not send all of the data from the preamble
+        proc.stdin.write(self.preamble[:-128])
+        proc.stdin.flush()
+        proc.stdin.close()
+        try:
+            returncode = proc.wait(timeout=10)
+        except testlib.subprocess.TimeoutExpired:
+            proc.kill()
+            proc.wait(timeout=3)
+            self.fail("First stage did not handle EOF on STDIN")
+        try:
+            self.assertEqual(0, returncode)
+            self.assertEqual(
+                proc.stdout.read(),
+                mitogen.parent.BootstrapProtocol.EC0_MARKER + b("\n"),
+            )
+            self.assertIn(
+                b("Error -5 while decompressing data"),
+                proc.stderr.read(),
+            )
+        finally:
+            proc.stdout.close()
+            proc.stderr.close()
