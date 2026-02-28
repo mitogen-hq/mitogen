36a82f0c0e9b5ea265f6f03959a385f74b224978
Alex Willmer <alex@moreati.org.uk>
2026-02-28 01:12:46 +0000

    CI: Extract macOS Python 2.x debug
    macOS versions available on Github Actions don't have Python 2.x installed, so
    it's mainly here just in case.

--- .ci/localhost_ansible_tests.py
+++ .ci/localhost_ansible_tests.py
@@ -37,27 +37,6 @@ with ci_lib.Fold('machine_prep'):
 
     ci_lib.run("ansible-playbook -c local -i localhost, macos_localhost.yml")
 
-    cmd = ';'.join([
-        'from __future__ import print_function',
-        'import os, sys',
-        'print(sys.executable, os.path.realpath(sys.executable))',
-    ])
-    for interpreter in ['/usr/bin/python', '/usr/bin/python2', '/usr/bin/python2.7']:
-        print(interpreter)
-        try:
-            subprocess.call([interpreter, '-c', cmd])
-        except OSError as exc:
-            print(exc)
-
-        print(interpreter, 'with PYTHON_LAUNCHED_FROM_WRAPPER=1')
-        environ = os.environ.copy()
-        environ['PYTHON_LAUNCHED_FROM_WRAPPER'] = '1'
-        try:
-            subprocess.call([interpreter, '-c', cmd], env=environ)
-        except OSError as exc:
-            print(exc)
-
-
 with ci_lib.Fold('ansible'):
     os.chdir(ci_lib.ANSIBLE_TESTS_DIR)
     playbook = os.environ.get('PLAYBOOK', 'all.yml')
